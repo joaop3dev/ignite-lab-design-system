4f42b6baee0a8ba4afb612763b84573ddfeecd90
Asmodeus Demon <joaopaulo7l2015@gmail.com>
2022-10-13 20:32:37 -0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    fontSize: {
+      xs: '14px',
+      sm: '16px',
+      md: '18px',
+      lg: '20px',
+      xl: '24px',
+      '2xl': '32px',
+    },
+    colors: {
+      transparent: 'transparent',
+
+      black: '#000000',
+      white: '#ffffff',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      cyan: {
+        500: '#81d8f7',
+        300: '#9be1fb',
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
