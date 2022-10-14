c09fab72cf642cf4547ef65bea580301f91446ed
Asmodeus Demon <joaopaulo7l2015@gmail.com>
2022-10-14 01:10:37 -0300

    fixing main storybook folder base file 2x

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -18,7 +18,7 @@ module.exports = {
   },
   "viteFinal": (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/lab-ds/'
+      config.base = '/ignite-lab-design-system/'
     }
 
     return config;
